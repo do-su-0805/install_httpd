a89c76c4d906540a78501e0830ace0b42a066999
do-su-0805 <sasugasannann@gmail.com>
2017-12-03 21:13:46 +0900

    setting version by script && download_dir method

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 
+WORKING_DIR=$(cd $(dirname $0); pwd)
+DOWNLOAD_DIR="$(cd $WORKING_DIR/..;pwd)/src"
+echo $DOWNLOAD_DIR
+
+./setenv.sh
+
+if [ -d $DOWNLOAD_DIR ]; then
+  rm -rf $DOWNLOAD_DIR
+fi
+
+mkdir $DOWNLOAD_DIR && cd $DOWNLOAD_DIR
+
