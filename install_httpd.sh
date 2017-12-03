109d109e91cc9df73a9a8750991c45486b740664
do-su-0805 <sasugasannann@gmail.com>
2017-12-03 21:52:34 +0900

    checking exist curl

--- install_httpd.sh
+++ install_httpd.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+## checking for using this script
+
+CURL=`which curl`
+if [ $? -ne 0 ]; then
+  "Error: Not found CURL. please install CURL"
+  exit 1;
+fi
+
 WORKING_DIR=$(cd $(dirname $0); pwd)
 echo $WORKING_DIR
 
