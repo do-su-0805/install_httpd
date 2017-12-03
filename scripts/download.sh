eb2d3c000524ff596badaec92903e556b7e2b51c
do-su-0805 <sasugasannann@gmail.com>
2017-12-03 21:32:39 +0900

    source command add

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,7 +4,7 @@ WORKING_DIR=$(cd $(dirname $0); pwd)
 DOWNLOAD_DIR="$(cd $WORKING_DIR/..;pwd)/src"
 echo $DOWNLOAD_DIR
 
-./setenv.sh
+. ./setenv.sh
 
 if [ -d $DOWNLOAD_DIR ]; then
   rm -rf $DOWNLOAD_DIR
@@ -12,3 +12,16 @@ fi
 
 mkdir $DOWNLOAD_DIR && cd $DOWNLOAD_DIR
 
+# download scrpkgs
+
+#curl -O "http://ftp.jaist.ac.jp/pub/apache/apr/apr-${APR_VERSION}.tar.gz"
+echo "http://ftp.jaist.ac.jp/pub/apache/apr/apr-${APR_VERSION}.tar.gz"
+
+#curl -O "http://ftp.jaist.ac.jp/pub/apache/apr/apr-util-${APRUTIL_VERSION}.tar.gz"
+echo "http://ftp.jaist.ac.jp/pub/apache/apr/apr-util-${APRUTIL_VERSION}.tar.gz"
+
+#curl -O "https://ftp.pcre.org/pub/pcre/pcre-${PCRE_VERSION}.tar.gz"
+echo "https://ftp.pcre.org/pub/pcre/pcre-${PCRE_VERSION}.tar.gz"
+
+#curl -O "http://ftp.jaist.ac.jp/pub/apache/httpd/httpd-${HTTPD_VERSION}.tar.gz"
+echo "http://ftp.jaist.ac.jp/pub/apache/httpd/httpd-${HTTPD_VERSION}.tar.gz"
